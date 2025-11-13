7a8618ab8538222ab13471a83559c6278b157744
Khalkot <72662630+khalkot@user.noreply.github.com>
2025-11-13 23:09:46 +0200

    Remove unrelated content, Add counter and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
 
+
+
 function App() {
+const [counter, setCounter] = useState(0);
+
+const handlePlus = () => setCounter(counter+1);
+const handleMinus = () => setCounter(counter-1);
 
   return (
     <>
-    
+    <h1>{counter}</h1>
+    <button onClick={handlePlus} >+</button>
+    <button onClick={handleMinus} >-</button>
     </>
   )
 }
